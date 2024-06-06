aafb97aeb5c3afa5b70b9833adbdf34c7ac88847
min913 <123183864+min913@users.noreply.github.com>
2024-06-07 00:52:22 +0900

    Update Evaluation_Character-level.py

--- Calligraphy/Task & Evaluation/Evaluation_Character-level.py
+++ Calligraphy/Task & Evaluation/Evaluation_Character-level.py
@@ -6,7 +6,6 @@ def recall_precision_f1("predicted.csv"):
     df1 = pd.read_csv("\predicted.csv")
     df2 = pd.read_csv("\gt.csv")
 
-    # Recall 및 Precision 계산 함수
     def recall_and_precision(row):
         gpt_content = row['content_gpt']
         ocr_content = row['content_ocr']
@@ -14,10 +13,7 @@ def recall_precision_f1("predicted.csv"):
         len_gpt = len(gpt_content)
         len_ocr = len(ocr_content)
 
-        # 일치하는 글자 수 계산
         matches = sum(1 for gpt_char, ocr_char in zip(gpt_content, ocr_content) if gpt_char == ocr_char)
-
-        # Recall 및 Precision 계산
         recall = matches / len_gpt if len_gpt > 0 else 0
         precision = matches / len_ocr if len_ocr > 0 else 0
         
@@ -29,19 +25,29 @@ def recall_precision_f1("predicted.csv"):
         return pd.Series([recall, precision,f1])
 
 def cer(preds_csv_path, gt_csv_path):
-    # CSV 파일 읽기
     preds_df = pd.read_csv(preds_csv_path)
     gt_df = pd.read_csv(target_csv_path)
-
-    # filename을 기준으로 데이터 매칭
     merged_df = pd.merge(preds_df, target_df, on='filename', suffixes=('_pred', '_gt'))
-
-    # 예측값과 타겟값 리스트 생성
     preds = merged_df[preds_column + '_pred'].tolist()
     gt = merged_df[target_column + '_gt'].tolist()
 
-    # WER 계산
+
     cer = CharErrorRate()
-    cer_score = cer(preds, gt).item()  # .item()을 사용하여 텐서 값을 가져옵니다.
+    cer_score = cer(preds, gt).item()  
 
     return cer_score
+
+def LDMETRIC(preds_csv_path, gt_csv_path):
+    df1 = pd.read_csv(preds_csv_path)
+    df2 = pd.read_csv(gt_csv_path)
+    merged_df = pd.merge(df1, df2, on='filename', suffixes=('_gpt', '_ocr'))
+
+
+    def levenshtein_distance(row):
+        return distance(row['content_gpt'], row['content_ocr'])
+
+    merged_df['levenshtein_distance'] = merged_df.apply(levenshtein_distance, axis=1)
+    average_ld = merged_df['levenshtein_distance'].mean()
+
+    return average_ld
+
